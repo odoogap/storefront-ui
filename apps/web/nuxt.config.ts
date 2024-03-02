fd940be0c7477f1cb9fc59b74e75ca32bddb4996
Diogo Duarte <dduarte@odoogap.com>
2024-03-02 19:45:43 +0000

    fix: redis credentials and storage

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -2,56 +2,56 @@
 export default defineNuxtConfig({
   app: {
     head: {
-      viewport: 'minimum-scale=1, initial-scale=1, width=device-width',
-      title: 'Vue Storefront',
+      viewport: "minimum-scale=1, initial-scale=1, width=device-width",
+      title: "Vue Storefront",
       htmlAttrs: {
-        lang: 'en',
+        lang: "en",
       },
     },
   },
   delayHydration: {
-    mode: 'init',
+    mode: "init",
   },
   components: {
     dirs: [
       {
         isAsync: true,
-        path: 'components',
+        path: "components",
       },
     ],
   },
   modules: [
-    'nuxt-lazy-hydrate',
-    '@nuxtjs/critters',
-    '@nuxtjs/tailwindcss',
-    '@nuxt/image',
-    'nuxt-delay-hydration',
+    "nuxt-lazy-hydrate",
+    "@nuxtjs/critters",
+    "@nuxtjs/tailwindcss",
+    "@nuxt/image",
+    "nuxt-delay-hydration",
     [
-      '@nuxtjs/google-fonts',
+      "@nuxtjs/google-fonts",
       {
         families: {
-          'Red Hat Display': [400, 500, 700],
+          "Red Hat Display": [400, 500, 700],
         },
       },
     ],
-    '@nuxtjs/fontaine',
+    "@nuxtjs/fontaine",
     [
-      '@nuxtjs/i18n',
+      "@nuxtjs/i18n",
       {
         locales: [
           {
-            code: 'en',
-            file: 'en.json',
+            code: "en",
+            file: "en.json",
           },
         ],
-        strategy: 'no_prefix',
+        strategy: "no_prefix",
         lazy: true,
-        langDir: 'lang',
-        defaultLocale: 'en',
+        langDir: "lang",
+        defaultLocale: "en",
       },
     ],
     // '@storyblok/nuxt',
-    'nuxt-lodash',
+    "nuxt-lodash",
   ],
   // storyblok: {
   //   accessToken: process.env.NUXT_STORYBLOK_TOKEN,
@@ -61,7 +61,7 @@ export default defineNuxtConfig({
   // },
   image: {
     screens: {
-      '2xl': 1536,
+      "2xl": 1536,
       xxl: 1440,
       xl: 1280,
       lg: 1024,
@@ -72,14 +72,14 @@ export default defineNuxtConfig({
   },
   build: {
     transpile: [
-      'tslib',
-      '@apollo/client',
-      '@apollo/client/core',
-      '@vue/apollo-composable',
-      '@vue/apollo-option',
-      'ts-invariant',
-      'vue-toastification',
-      '@erpgap/odoo-sdk-api-client',
+      "tslib",
+      "@apollo/client",
+      "@apollo/client/core",
+      "@vue/apollo-composable",
+      "@vue/apollo-option",
+      "ts-invariant",
+      "vue-toastification",
+      "@erpgap/odoo-sdk-api-client",
     ],
   },
   devtools: {
@@ -87,19 +87,25 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     redis: {
-      host: 'localhost',
+      host: "localhost",
       port: 6379,
     },
     public: {
-      odooBaseImageUrl: '',
-      odooBaseUrl: '',
+      odooBaseImageUrl: "",
+      odooBaseUrl: "",
     },
   },
   routeRules: {
-    '/': { swr: true },
-    '/_ipx/**': { headers: { 'cache-control': 'public, max-age=31536000, immutable' } },
-    '/icons/**': { headers: { 'cache-control': 'public, max-age=31536000, immutable' } },
-    '/favicon.ico': { headers: { 'cache-control': 'public, max-age=31536000, immutable' } },
+    "/": { swr: true },
+    "/_ipx/**": {
+      headers: { "cache-control": "public, max-age=31536000, immutable" },
+    },
+    "/icons/**": {
+      headers: { "cache-control": "public, max-age=31536000, immutable" },
+    },
+    "/favicon.ico": {
+      headers: { "cache-control": "public, max-age=31536000, immutable" },
+    },
   },
   nitro: {
     // publicAssets: [
@@ -112,25 +118,27 @@ export default defineNuxtConfig({
     compressPublicAssets: true,
     storage: {
       cache: {
-        driver: 'redis',
+        driver: "redis",
+        url: process.env.REDIS_URL,
       },
     },
     devStorage: {
       cache: {
-        driver: 'redis',
+        driver: "redis",
+        url: process.env.REDIS_URL,
       },
     },
   },
   vite: {
     optimizeDeps: {
-      include: ['lodash-es'],
+      include: ["lodash-es"],
     },
   },
   image: {
     providers: {
       odooProvider: {
-        name: 'odooProvider',
-        provider: '~/providers/odoo-provider.ts',
+        name: "odooProvider",
+        provider: "~/providers/odoo-provider.ts",
       },
     },
   },
