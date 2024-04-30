73b0430931dc396da3c88827cb71383fb8d58120
Gideon Assafuah <gassafuah@odoogap.com>
2024-04-30 06:48:08 -0400

    [IMP] fetch all products from odoo to product.json every hour

--- apps/web/pages/category/[id].vue
+++ apps/web/pages/category/[id].vue
@@ -1,22 +1,34 @@
 <script setup lang="ts">
-import { SfButton, SfIconTune, useDisclosure, SfLoaderCircular } from '@storefront-ui/vue';
-import { Product } from '~/graphql';
+import {
+  SfButton,
+  SfIconTune,
+  useDisclosure,
+  SfLoaderCircular,
+} from "@storefront-ui/vue";
+import { Product } from "~/graphql";
 
 const route = useRoute();
 
 const { isOpen, open, close } = useDisclosure();
-const { loadProductTemplateList, organizedAttributes, loading, productTemplateList, totalItems, categories } =
-  useProductTemplateList(String(route.fullPath));
+const {
+  loadProductTemplateList,
+  organizedAttributes,
+  loading,
+  productTemplateList,
+  totalItems,
+  categories,
+} = useProductTemplateList(String(route.fullPath));
 const { getRegularPrice, getSpecialPrice } = useProductAttributes();
 const { getFacetsFromURL } = useUiHelpers();
 
 const breadcrumbs = [
-  { name: 'Home', link: '/' },
-  { name: 'Category', link: `Category/${route.params.id}` },
+  { name: "Home", link: "/" },
+  { name: "Category", link: `Category/${route.params.id}` },
 ];
 
 const maxVisiblePages = ref(1);
-const setMaxVisiblePages = (isWide: boolean) => (maxVisiblePages.value = isWide ? 5 : 1);
+const setMaxVisiblePages = (isWide: boolean) =>
+  (maxVisiblePages.value = isWide ? 5 : 1);
 
 watch(isWideScreen, (value) => setMaxVisiblePages(value));
 watch(isTabletScreen, (value) => {
@@ -30,7 +42,7 @@ watch(
   async () => {
     await loadProductTemplateList(getFacetsFromURL(route.query));
   },
-  { deep: true, immediate: true },
+  { deep: true, immediate: true }
 );
 
 const pagination = computed(() => ({
@@ -48,10 +60,16 @@ onMounted(() => {
 <template>
   <div class="pb-20">
     <UiBreadcrumb :breadcrumbs="breadcrumbs" class="self-start mt-5 mb-14" />
-    <h1 class="font-bold typography-headline-3 md:typography-headline-2 mb-10">All products</h1>
+    <h1 class="font-bold typography-headline-3 md:typography-headline-2 mb-10">
+      All products
+    </h1>
     <div class="grid grid-cols-12 lg:gap-x-6">
       <div class="col-span-12 lg:col-span-4 xl:col-span-3">
-        <CategoryFilterSidebar class="hidden lg:block" :attributes="organizedAttributes" :categories="categories" />
+        <CategoryFilterSidebar
+          class="hidden lg:block"
+          :attributes="organizedAttributes"
+          :categories="categories"
+        />
         <LazyCategoryMobileSidebar :is-open="isOpen" @close="close">
           <template #default>
             <CategoryFilterSidebar
@@ -66,8 +84,14 @@ onMounted(() => {
       <div class="col-span-12 lg:col-span-8 xl:col-span-9">
         <template v-if="!loading">
           <div class="flex justify-between items-center mb-6">
-            <span class="font-bold font-headings md:text-lg">{{ totalItems }} Products </span>
-            <SfButton variant="tertiary" class="lg:hidden whitespace-nowrap" @click="open">
+            <span class="font-bold font-headings md:text-lg"
+              >{{ totalItems }} Products
+            </span>
+            <SfButton
+              variant="tertiary"
+              class="lg:hidden whitespace-nowrap"
+              @click="open"
+            >
               <template #prefix>
                 <SfIconTune />
               </template>
@@ -83,11 +107,26 @@ onMounted(() => {
               :key="productTemplate.id"
               :name="productTemplate?.name || ''"
               loading="eager"
-              :slug="mountUrlSlugForProductVariant((productTemplate.firstVariant || productTemplate) as Product)"
-              :image-url="$getImage(String(productTemplate.image), 370, 370, String(productTemplate.imageFilename))"
+              :slug="
+                mountUrlSlugForProductVariant(
+                  (productTemplate.firstVariant || productTemplate) as Product
+                )
+              "
+              :image-url="
+                $getImage(
+                  String(productTemplate.image),
+                  370,
+                  370,
+                  String(productTemplate.imageFilename)
+                )
+              "
               :image-alt="productTemplate?.name || ''"
-              :regular-price="getRegularPrice(productTemplate.firstVariant as Product) || 250"
-              :special-price="getSpecialPrice(productTemplate.firstVariant as Product)"
+              :regular-price="
+                getRegularPrice(productTemplate.firstVariant as Product) || 250
+              "
+              :special-price="
+                getSpecialPrice(productTemplate.firstVariant as Product)
+              "
               :rating-count="123"
               :rating="Number(4)"
               :first-variant="productTemplate.firstVariant as Product"
