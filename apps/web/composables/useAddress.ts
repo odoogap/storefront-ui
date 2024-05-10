5648dfac29178cc06a76dbf54bc649b2f2a65de1
Lucas Soares <lsoares@odoogap.com>
2024-05-10 17:08:16 -0300

    [IMPL] implement account shipping address

--- apps/web/composables/useAddress.ts
+++ apps/web/composables/useAddress.ts
@@ -27,14 +27,6 @@ export const useAddresses = () => {
   const toast = useToast();
   const billingAddresses = useState<Partner[]>("billing-addresses", () => []);
   const mailingAddresses = useState<Partner[]>("mailing-addresses", () => []);
-  const currentBillingAddress = useState<Partner>(
-    "current-billing-address",
-    () => ({}) as Partner
-  );
-  const currentMailingAddress = useState<Partner>(
-    "current-mailing-address",
-    () => ({}) as Partner
-  );
 
   const loadBillingAddresses = async () => {
     loading.value = true;
@@ -77,21 +69,23 @@ export const useAddresses = () => {
 
   const addAddress = async (address: AddAddressInput, type: AddressEnum) => {
     loading.value = true;
-    try {
-      const { data } = await $sdk().odoo.mutation<
-        MutationAddAddressArgs,
-        AddAddressResponse
-      >({ mutationName: MutationName.AddAddress }, { address, type });
-      if (data.value) {
-        if (type === AddressEnum.Billing) {
-          billingAddresses.value.push(data.value.addAddress);
-        } else {
-          mailingAddresses.value.push(data.value.addAddress);
-        }
-      }
-    } finally {
-      loading.value = false;
+
+    const { data, error } = await $sdk().odoo.mutation<
+      MutationAddAddressArgs,
+      AddAddressResponse
+    >({ mutationName: MutationName.AddAddress }, { address, type });
+
+    if (error.value) {
+      return toast.error(error.value.data.message);
     }
+    if (type === AddressEnum.Billing) {
+      loadBillingAddresses();
+    } else {
+      loadShippingAddresses();
+    }
+
+    toast.success("Address has been successfully saved");
+    loading.value = false;
   };
 
   const deleteAddress = async (address: DeleteAddressInput) => {
@@ -105,9 +99,6 @@ export const useAddresses = () => {
     if (error.value) {
       return toast.error(error.value.data.message);
     }
-
-    loadBillingAddresses();
-    loadShippingAddresses();
     toast.success("Address has been successfully removed");
     loading.value = false;
   };
@@ -158,11 +149,9 @@ export const useAddresses = () => {
       }
 
       if (type === AddressEnum.Billing) {
-        currentBillingAddress.value = data.value;
         toast.success("Current billing address saved successfully");
         return;
       }
-      currentMailingAddress.value = data.value;
       toast.success("Current mailing address saved successfully");
     } finally {
       loading.value = false;
@@ -174,8 +163,6 @@ export const useAddresses = () => {
     loadShippingAddresses,
     billingAddresses,
     mailingAddresses,
-    currentBillingAddress,
-    currentMailingAddress,
     selectCurrentAddress,
     addAddress,
     updateAddress,
