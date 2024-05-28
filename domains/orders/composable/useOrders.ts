488e938789eff54fa3bcc63e920a7e6cd79fa0ab
iferreira <iferreira@odoogap.com>
2024-05-27 21:29:20 -0300

    feat: 🎸 get order by id api

--- domains/orders/composable/useOrders.ts
+++ domains/orders/composable/useOrders.ts
@@ -1,10 +1,17 @@
-import type { GetOrdersResponse, Orders } from "~/graphql";
+import type {
+  GetOrderResponse,
+  GetOrdersResponse,
+  Order,
+  Orders,
+  QueryOrderArgs,
+} from "~/graphql";
 import { QueryName } from "~/server/queries";
 
 export const useOrders = () => {
   const { $sdk } = useNuxtApp();
   const loading = ref(false);
   const orders = ref<Orders>();
+  const order = ref<Order>();
 
   const getOrders = async () => {
     loading.value = true;
@@ -16,9 +23,21 @@ export const useOrders = () => {
     orders.value = (data.value?.orders as Orders) || {};
   };
 
+  const getOrderById = async (params: QueryOrderArgs) => {
+    loading.value = true;
+    const { data } = await $sdk().odoo.query<QueryOrderArgs, GetOrderResponse>(
+      { queryName: QueryName.GetOrderQuery },
+      params
+    );
+    loading.value = false;
+    order.value = data?.value?.order || ({} as Order);
+  };
+
   return {
     loading,
     getOrders,
+    getOrderById,
     orders,
+    order,
   };
 };
