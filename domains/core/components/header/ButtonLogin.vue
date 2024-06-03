dd101bf4bcfbbb15380c802561a5379a5d95f774
Leonardo Muniz <lmuniz@odoogap.com>
2024-06-03 19:45:29 -0300

    fix: 🐛 button login

--- domains/core/components/header/ButtonLogin.vue
+++ domains/core/components/header/ButtonLogin.vue
@@ -7,22 +7,22 @@ const { isAuthenticated } = useAuth();
 </script>
 
 <template>
-  <ClientOnly>
-    <SfButton
-      class="text-white hover:text-white active:text-white hover:bg-primary-800 active:bg-primary-900 mr-1 -ml-0.5 rounded-md"
-      variant="tertiary"
-      :to="isAuthenticated ? '/my-account/personal-data' : '/login'"
-      :tag="NuxtLink"
-      square
-    >
-      <template #prefix>
-        <Icon
-          :name="isAuthenticated ? 'ion:person' : 'ion:person-outline'"
-          size="22px"
-        />
-      </template>
-    </SfButton>
-
+  <!-- <ClientOnly> -->
+  <SfButton
+    class="text-white hover:text-white active:text-white hover:bg-primary-800 active:bg-primary-900 mr-1 -ml-0.5 rounded-md"
+    variant="tertiary"
+    :to="isAuthenticated ? '/my-account/personal-data' : '/login'"
+    :tag="NuxtLink"
+    square
+  >
+    <template #prefix>
+      <Icon
+        :name="isAuthenticated ? 'ion:person' : 'ion:person-outline'"
+        size="22px"
+      />
+    </template>
+  </SfButton>
+  <!-- 
     <template #fallback>
       <SfButton
         class="group relative text-white hover:text-white active:text-white hover:bg-primary-800 active:bg-primary-900 rounded-md"
@@ -34,5 +34,5 @@ const { isAuthenticated } = useAuth();
         </template>
       </SfButton>
     </template>
-  </ClientOnly>
+  </ClientOnly> -->
 </template>
