38064408da02892a15a8fd582ef6cdb3818b661d
Lucas Soares <lsoares@odoogap.com>
2024-08-12 21:36:48 -0300

    feat: 🎸 WIP add discount and gift card component to checkout
    ✅ Closes: 19703

---
+++ domains/checkout/components/CheckoutDiscountGiftcard.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { SfButton, SfInput } from '@storefront-ui/vue';
+const value = ref('');
+</script>
+
+<template>
+  <div class="w-full py-4 flex items-center gap-4">
+    <div class="w-full">
+      <SfInput
+        v-model="value"
+        size="lg"
+        placeholder="Discount code or gift card"
+      />
+    </div>
+
+    <SfButton :disabled="!value" size="lg" variant="secondary">
+      Apply
+    </SfButton>
+  </div>
+</template>
+
+<style scoped></style>
