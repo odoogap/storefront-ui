748ad0c75473d407087bc76536275f757a5a8ca1
iferreira <iferreira@odoogap.com>
2024-09-23 12:34:53 -0300

    feat: 🎸 new page reset password success content

--- domains/auth/pages/reset-password-success.vue
+++ domains/auth/pages/reset-password-success.vue
@@ -3,31 +3,19 @@
     <div
       class="mx-auto text-center rounded-md border border-neutral-200 p-4 md:p-6 mt-2"
     >
-      <NuxtImg
-        src="/images/email-sent.png"
-        :alt="$t('auth.resetPassword.emailSent')"
-        width="192"
-        height="192"
-        class="mx-auto mb-6"
-      />
-      <h1 class="typography-headline-3 font-bold">
-        {{ $t("auth.resetPassword.heading") }}
-      </h1>
-      <i18n-t
-        tag="p"
-        keypath="auth.resetPassword.thankYou"
-        class="bg-neutral-100 text-left border border-neutral-200 p-4 rounded-md my-4"
+      <div
+        class="w-full min-h-[330px] flex flex-col gap-4 items-center justify-center"
       >
-        <span class="font-medium">{{ email }}</span>
-      </i18n-t>
-      <SfButton
-        to="/reset-password"
-        :tag="NuxtLink"
-        variant="tertiary"
-        data-testid="reset-password-page-reset-button"
-      >
-        {{ $t("auth.resetPassword.anotherEmail") }}
-      </SfButton>
+        <Icon
+          name="i-material-symbols-check-circle"
+          class="size-20 text-primary-700"
+        />
+        <h1 class="text-3xl font-bold">Your request has ben sent!</h1>
+        <p>You just received a reply in your registered email.</p>
+        <SfButton @click="navigateTo('/')" size="lg" variant="secondary">
+          Continue shopping
+        </SfButton>
+      </div>
     </div>
   </NuxtLayout>
 </template>
